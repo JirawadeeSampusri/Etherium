3ded98ad032eb0711bae1f8735b764b5dbce32a9
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2021-02-27 00:10:40 +0700

    Updates

---
+++ precache-manifest.b2dc7ecdc74724fc93bb51ead53df847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1242f0c39ce6f52daa8e",
+    "url": "/Etherium/static/css/main.12614253.chunk.css"
+  },
+  {
+    "revision": "1242f0c39ce6f52daa8e",
+    "url": "/Etherium/static/js/main.1242f0c3.chunk.js"
+  },
+  {
+    "revision": "7ef3347734de46553ab0",
+    "url": "/Etherium/static/css/1.d6f16a40.chunk.css"
+  },
+  {
+    "revision": "7ef3347734de46553ab0",
+    "url": "/Etherium/static/js/1.7ef33477.chunk.js"
+  },
+  {
+    "revision": "bc64548795d4a45868c5",
+    "url": "/Etherium/static/js/runtime~main.bc645487.js"
+  },
+  {
+    "revision": "23d9824510c7e1a02102f5ebc923579d",
+    "url": "/Etherium/index.html"
+  }
+];
